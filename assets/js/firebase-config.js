a0ee66867ffe6a47c04d9aebb12c5dee49bc0772
avpxxd <andrewlveinotte@gmail.com>
2026-04-22 15:12:46 -0300

    Restore Firebase config values

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,7 +1,9 @@
 window.FIREBASE_CONFIG = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAU95e7JSmMb54hCWlcgvS5lL0RW56EcoI",
+  authDomain: "planes-dvgames.firebaseapp.com",
+  projectId: "planes-dvgames",
+  storageBucket: "planes-dvgames.firebasestorage.app",
+  messagingSenderId: "298487362471",
+  appId: "1:298487362471:web:2a3da34162ea79c2253c82",
+  databaseURL: "https://planes-dvgames-default-rtdb.firebaseio.com"
 };
